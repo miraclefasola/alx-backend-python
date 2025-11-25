b48727765f30a5fee54b679696e70f1ca8419f68
Miracle Fasola <Fasolamiracle001@gmail.com>
2025-11-25 17:30:29 +0100

    Simulte fetching paginated data from the users database using a generator to lazily load each page

---
+++ python-generators-0x00/2-lazy_paginate.py
@@ -0,0 +1,30 @@
+import mysql.connector
+
+def paginate_users(page_size, offset):
+    """Fetch a single page of users from the database."""
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="password",
+        database="ALX_prodev"
+    )
+    cursor = connection.cursor()
+    query = f"SELECT * FROM user_data ORDER BY user_id LIMIT {page_size} OFFSET {offset};"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return rows
+
+
+def lazy_paginate(page_size):
+    """Generator that lazily fetches users page by page."""
+    offset = 0
+
+    while True:
+        page = paginate_users(page_size, offset)
+        if not page:
+            break
+        for row in page:
+            yield row
+        offset += page_size
