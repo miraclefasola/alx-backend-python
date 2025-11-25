9b298cdf96019406b42903f9807374ce92ffb7dd
Miracle Fasola <Fasolamiracle001@gmail.com>
2025-11-25 16:27:16 +0100

    Added  a function that uses a generator to fetch rows one by one from the user_data table

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -1,27 +1,29 @@
 import mysql.connector
 
-def connectdb():
+def stream_users():    
     try:
         connection=mysql.connector.connect(
             host="localhost",
             user="root",
             password="password",
         )
-        return connection
+        cursor=connection.cursor()
+        cursor.execute("USE ALX_prodev;")
+        cursor.execute("SELECT * from user_data")
+
+        for row in cursor:
+            yield row
+
+        cursor.close()
+
     except mysql.connector.Error as e:
         print(F"Error during connection {e}")
         return None
 
-def stream_users(connection):
-    cursor=connection.cursor()
-    cursor.execute("USE ALX_prodev;")
-    cursor.execute("SELECT * from user_data")
-
-    for row in cursor:
-        yield row
+    
+    finally:
+        if connection.is_connected():
+            connection.close()
 
 
-con = connectdb()
 
-for user in stream_users(con):
-    print(user)
