62151226796eadeeacf569a0f1f2372cb2d9de12
Miracle Fasola <Fasolamiracle001@gmail.com>
2025-11-26 18:18:25 +0100

    added a function that database queries concurrently using asyncio.gather.

---
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -0,0 +1,24 @@
+import aiosqlite
+import asyncio
+
+
+async def async_fetch_users():
+    async with aiosqlite.connect("alx_database") as db:
+        async with db.execute("SELECT * FROM users") as cursor:
+            result= [row async for row in cursor]
+            print(result)
+    
+async def async_fetch_older_users():
+    async with aiosqlite.connect("alx_database") as db:
+        async with db.execute("SELECT * FROM users WHERE age > 40") as cursor:
+            result=[row async for row in cursor]
+            print(result)
+
+
+async def fetch_concurrently():
+    await asyncio.gather(
+        async_fetch_users(),
+        async_fetch_older_users()
+    )
+
+asyncio.run(fetch_concurrently())
