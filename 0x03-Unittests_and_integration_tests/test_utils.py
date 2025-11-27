9daa0fa2d951f83954d4283602a1aa1cd4fc27f1
Miracle Fasola <Fasolamiracle001@gmail.com>
2025-11-27 22:01:43 +0100

    unit test integration

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -77,13 +77,13 @@ class TestMemoize(unittest.TestCase):
 
         instance = TestClass()
 
-        with patch.object(TestClass, "a_method", return_value=42) as mock_method:
+        with patch.object(TestClass, "a_method", return_value=42) as mock_time:
             result1 = instance.a_property
             result2 = instance.a_property
 
             self.assertEqual(result1, 42)
             self.assertEqual(result2, 42)
-            mock_method.assert_called_once()
+            mock_time.assert_called_once()
 
 
 if __name__ == "__main__":
