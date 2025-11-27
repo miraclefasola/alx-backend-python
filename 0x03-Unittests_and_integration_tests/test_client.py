22d9168ff54762fbcacea60e9f0905d46527bbe3
Miracle Fasola <Fasolamiracle001@gmail.com>
2025-11-27 22:11:20 +0100

    unit test integration

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Test module for GithubOrgClient"""
+
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Unit tests for GithubOrgClient"""
+
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name, mock_get_json):
+        """Test that GithubOrgClient.org returns the correct payload"""
+
+        # Fake payload for the org
+        expected_payload = {"org": org_name}
+        mock_get_json.return_value = expected_payload
+
+        client = GithubOrgClient(org_name)
+        result = client.org
+
+        # Assertions
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}"
+        )
+        self.assertEqual(result, expected_payload)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -86,29 +86,6 @@ class TestMemoize(unittest.TestCase):
             self.assertEqual(result2, 42)
             mock_time.assert_called_once()
 
-class TestGithubOrgClient(unittest.TestCase):
-    """Unit tests for GithubOrgClient"""
-
-    @parameterized.expand([
-        ("google",),
-        ("abc",),
-    ])
-    @patch('client.get_json')
-    def test_org(self, org_name, mock_get_json):
-        """Test that GithubOrgClient.org returns the correct payload"""
-
-        # Fake payload for the org
-        expected_payload = {"org": org_name}
-        mock_get_json.return_value = expected_payload
-
-        client = GithubOrgClient(org_name)
-        result = client.org
-
-        # Assertions
-        mock_get_json.assert_called_once_with(
-            f"https://api.github.com/orgs/{org_name}"
-        )
-        self.assertEqual(result, expected_payload)
 
 
 if __name__ == "__main__":
