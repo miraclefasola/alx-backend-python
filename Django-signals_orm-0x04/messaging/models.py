bfc32619bf0548d068f23d367f08ad578f4f2d5a
Miracle Fasola <Fasolamiracle001@gmail.com>
2025-12-04 14:05:53 +0100

    Log when a user edits a message and save the old content before the edit.

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -15,6 +15,26 @@ class Message(models.Model):
     )
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    edited = models.BooleanField(default=False) 
+
+class MessageHistory(models.Model):
+    """Stores previous versions of a message before edits"""
+
+    message = models.ForeignKey(
+        Message, related_name="history", on_delete=models.CASCADE
+    )
+    old_content = models.TextField()
+    edited_at = models.TimeField(auto_now_add=True)
+    edited_by = models.ForeignKey(  # ✅ who made the edit
+        settings.AUTH_USER_MODEL,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="message_edits",
+    )
+
+    def __str__(self):
+        return f"History of Message {self.message.id} at {self.edited_at}"
 
 
 
@@ -27,6 +47,7 @@ class Notification(models.Model):
     )
     is_read = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return f"Notification for {self.user.username} - Message ID {self.message.id}"
--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save, pre_save, post_delete
 from django.dispatch import receiver
-from .models import Message, Notification
+from .models import Message, Notification, MessageHistory
 from django.contrib.auth import get_user_model
 
 
@@ -12,3 +12,24 @@ def create_notification(sender, instance, created, **kwargs):
     """Create a notification for the receiver when a new message is sent."""
     if created:
         Notification.objects.create(user=instance.receiver, message=instance)
+
+
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    """Log old content before a message is updated"""
+    if not instance.pk:  # skip if it's a new message
+        return
+
+    try:
+        old_message = Message.objects.get(pk=instance.pk)
+    except Message.DoesNotExist:
+        return
+
+    # If content changed, log it in history
+    if old_message.content != instance.content:
+        MessageHistory.objects.create(
+            message=instance,
+            old_content=old_message.content,
+            edited_by=instance.edited_by,  # ✅ log who did the edit
+        )
+        instance.edited = True
