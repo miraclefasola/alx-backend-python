4354a6faafd095b0d1cc53a395468131a1928283
Miracle Fasola <Fasolamiracle001@gmail.com>
2025-12-04 15:25:14 +0100

    Set up basic caching for a view that retrieves messages in the messaging app.

--- Django-signals_orm-0x04/messaging_app/settings.py
+++ Django-signals_orm-0x04/messaging_app/settings.py
@@ -163,3 +163,10 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
